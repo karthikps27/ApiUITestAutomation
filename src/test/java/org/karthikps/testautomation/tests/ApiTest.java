1376292bcb8b57d9539e729be7b611582e534d91
Karthik Soorya <karthikps27@gmail.com>
2020-06-02 22:28:45 +1000

    Continue build upon test failures

--- src/test/java/org/karthikps/testautomation/tests/ApiTest.java
+++ src/test/java/org/karthikps/testautomation/tests/ApiTest.java
@@ -18,11 +18,16 @@ public class ApiTest {
     @Test(groups = {"Tests"}, description = "Signup and Login Tests")
     @Description("Signup and Login Tests")
     public void SignupAndLogin() {
-        UserAccountApiUtils accountApiUtils = new UserAccountApiUtils(TestProperties.getPropertyValue("api.baseURL"));
-        Response response = accountApiUtils.UserSignupRequest();
-        logger.info("User signup status: " + response.getStatusCode());
-        logger.info("Signup response: " + response.getBody().asString());
-        Assert.assertEquals(response.getStatusCode(), HttpStatus.SC_OK);
-        Assert.assertEquals(accountApiUtils.evaluateResponseWithJsonPath(response,"Success"), "true");
+        try {
+            UserAccountApiUtils accountApiUtils = new UserAccountApiUtils(TestProperties.getPropertyValue("api.baseURL"));
+            Response response = accountApiUtils.UserSignupRequest();
+            logger.info("User signup status: " + response.getStatusCode());
+            logger.info("Signup response: " + response.getBody().asString());
+            Assert.assertEquals(response.getStatusCode(), HttpStatus.SC_OK);
+            Assert.assertTrue(response.jsonPath().get("Success"));
+        }
+        catch (Exception e) {
+            Assert.fail(e.getMessage());
+        }
     }
 }
